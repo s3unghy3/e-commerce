7a54eefe68ea2ffb2efc59c4ff1df20961367d64
s3unghy3 <seunghyeyang@yahoo.com>
2021-09-15 00:46:52 +0200

    client checkout done

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { LoadingInterceptor } from './core/Interceptors/loading.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { LoadingInterceptor } from './core/Interceptors/loading.interceptor';
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
